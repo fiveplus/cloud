76d88610007c4eec11e625b35cdefc66a1efa32e
nobmr <274925658@qq.com>
2017-03-03 14:58:54 +0800

    upt:实体类Praise数据绑定

--- src/main/java/com/cloud/entity/Praise.java
+++ src/main/java/com/cloud/entity/Praise.java
@@ -1,6 +1,10 @@
 package com.cloud.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -10,24 +14,34 @@ public class Praise {
 	private Integer contentId;
 	private Integer userId;
 	private Long createTime;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", unique = true, nullable = false)
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
+	@Column(name = "CONTENT_ID")
 	public Integer getContentId() {
 		return contentId;
 	}
 	public void setContentId(Integer contentId) {
 		this.contentId = contentId;
 	}
+	
+	@Column(name = "USER_ID")
 	public Integer getUserId() {
 		return userId;
 	}
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+	
+	@Column(name = "CREATE_TIME")
 	public Long getCreateTime() {
 		return createTime;
 	}
