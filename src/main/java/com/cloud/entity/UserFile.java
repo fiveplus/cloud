fa488e0e81546e98029486e5eff39ec8c7b2dd65
nobmr <274925658@qq.com>
2017-04-07 09:25:59 +0800

    add:新增实体类UserFile.java

---
+++ src/main/java/com/cloud/entity/UserFile.java
@@ -0,0 +1,76 @@
+package com.cloud.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_FILE")
+public class UserFile implements Serializable{
+	private Integer id;
+	private String fileName;
+	private String url;
+	private Long createTime;
+	private User user;
+	private Content cont;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "FILE_NAME")
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	@Column(name = "URL")
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	@Column(name = "CREATE_TIME")
+	public Long getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+	
+	@OneToOne
+	@JoinColumn(name="USER_ID")
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@OneToOne
+	@JoinColumn(name="CONTENT_ID")
+	public Content getCont() {
+		return cont;
+	}
+	public void setCont(Content cont) {
+		this.cont = cont;
+	}
+	
+}
