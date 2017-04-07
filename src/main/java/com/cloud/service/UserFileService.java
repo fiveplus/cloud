5c73c28e2081361f68b834b919408175db8a2a29
nobmr <274925658@qq.com>
2017-04-07 09:31:08 +0800

    add:新增UserFileService.java

---
+++ src/main/java/com/cloud/service/UserFileService.java
@@ -0,0 +1,10 @@
+package com.cloud.service;
+
+import org.springframework.stereotype.Service;
+
+import com.cloud.entity.UserFile;
+
+@Service("userFileService")
+public class UserFileService extends BaseService<UserFile, Integer>{
+	
+}
