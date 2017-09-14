2e55616901cf99c6330adfbe05bd02cbfe34c432
nobmr <274925658@qq.com>
2017-09-14 16:19:39 +0800

    add:新增实体类MailQuartz

---
+++ src/main/java/com/cloud/entity/MailQuartz.java
@@ -0,0 +1,91 @@
+package com.cloud.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SYS_MAIL_QUARTZ")
+public class MailQuartz {
+	private Integer id;
+	private String jobName;
+	private String cron;
+	private User user;
+	private Long createTime;
+	private String title;
+	private String content;
+	private String status;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "JOB_NAME")
+	public String getJobName() {
+		return jobName;
+	}
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+	
+	@Column(name = "CRON")
+	public String getCron() {
+		return cron;
+	}
+	public void setCron(String cron) {
+		this.cron = cron;
+	}
+	
+	@OneToOne
+	@JoinColumn(name="USER_ID")
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Column(name = "CREATE_TIME")
+	public Long getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+	
+	@Column(name = "TITLE")
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Column(name = "CONTENT")
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Column(name = "STATUS")
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
