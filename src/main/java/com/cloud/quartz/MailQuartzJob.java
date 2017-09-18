d5798cb660c4528a2388b975010c08a6b970d15f
nobmr <274925658@qq.com>
2017-09-18 17:04:16 +0800

    add:autowired重新注入。

--- src/main/java/com/cloud/quartz/MailQuartzJob.java
+++ src/main/java/com/cloud/quartz/MailQuartzJob.java
@@ -1,24 +1,21 @@
 package com.cloud.quartz;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.quartz.Job;
-import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import com.cloud.controller.ApplicationController;
 import com.cloud.entity.MailQuartz;
 import com.cloud.entity.User;
 import com.cloud.service.MailQuartzService;
 import com.cloud.service.UserService;
 import com.cloud.util.MailThread;
-import com.cloud.util.StringUtil;
 
 /**
  * quartz定时器类
@@ -46,15 +43,17 @@ public class MailQuartzJob implements Job{
 		
 		MailQuartz mail = mailQuartzService.getMailQuartzToJobName(jobName);
 		
-		
-		
 		//TODO 此处进行相关邮件操作
 		//List<User> users = userService.findAll();
 		String title = mail.getTitle();
 		String content = mail.getContent();
 		LOGGER.info("title:"+title);
 		//TODO 开启邮件发送线程
-		//Thread mailThread = new MailThread(users, title, content);
-		//mailThread.start();
+		User user = new User();
+		user.setLoginName("274925658@qq.com");
+		List<User> users = new ArrayList<User>();
+		users.add(user);
+		Thread mailThread = new MailThread(users, title, content);
+		mailThread.start();
 	}
 }
